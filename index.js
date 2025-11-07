24d070eb4888291e0b7cea9486c4e1f467874df5
Mrunank Dekhane <mrunankdekhane@gmail.com>
2025-11-07 12:12:35 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const age = 24;
-const name = "mrunank";
+const name1 = "mrunank";
 
 function sayName(){
-  console.log("my name is "+ name);
+  console.log("my name is "+ name1);
+}
+
+function isAbove18(){
+  return age>= 18;
 }
